a39c7911928a11658fd7b7c4b124646831928ffa
charles <charlesaidanwatson@icloud.com>
2024-11-03 11:21:35 +0800

    gitignore + rust progress

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -3,8 +3,16 @@ fn init_board() -> [[i32; 3]; 3]{
 }
 
 fn print_board(board: [[i32; 3]; 3]){
+
+    println!("\t1\t2\t3");
+    println!(" {}", "-".repeat(25));
+
     for i in 0..3 {
-        println!("{:?}", board[i]);
+        print!("{}|\t", i + 1);
+        for j in board[i]{
+            print!("{}\t", j);
+        }
+        println!();
     }
 }
 
@@ -14,7 +22,10 @@ fn game_loop(board: [[i32; 3]; 3], player_ones_move: bool){
 
 fn main() {
     println!("Noughts and crosses, rust edition!\n");
+
+    // init game state variables
     let player_ones_move = true;
     let board: [[i32; 3];3] = init_board();
+
     game_loop(board, player_ones_move);
 }
